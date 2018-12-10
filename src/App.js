ebbb1c32e5714bbab3521fe9b6c521ecbc5e8f78
Patrick Monaghan <monaghanp3@gmail.com>
2018-12-10 16:52:32 -0500

    completed todo-list dapp

--- src/App.js
+++ src/App.js
@@ -1,35 +1,80 @@
-import React, { Component } from 'react'
-import { DisplayTodos, CreateTodoBtn } from './Components'
+import React, { Component } from 'react';
+import { DisplayTodos, CreateTodoBtn } from './Components';
+import Contract from 'truffle-contract';
+import TodoListContract from '../build/contracts/TodoList.json';
 
-import './css/oswald.css'
-import './css/open-sans.css'
-import './css/pure-min.css'
-import './App.css'
+import './css/oswald.css';
+import './css/open-sans.css';
+import './css/pure-min.css';
+import './App.css';
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       todoListInstance: {},
       todos: [],
-    }
+      account: '',
+    };
   }
 
-  componentDidMount(){
+  async componentDidMount() {
     /*
       GET THE WEB3 OBJECT HERE,
       INSTANTIATE SMART CONTRACT,
       GET TODOS
     */
+    // console.log(window.web3);
+
+    await this.instantiateContract();
+    await this.getAccount();
+    await this.getTodos();
+  }
+
+  async instantiateContract() {
+    const todoList = Contract(TodoListContract);
+    todoList.setProvider(window.web3.currentProvider);
+    const todoListInstance = await todoList.deployed();
+    this.setState({ todoListInstance });
+  }
+
+  getAccount() {
+    window.web3.eth.getAccounts((err, accounts) => {
+      const account = accounts[0];
+      this.setState({ account });
+      window.web3.eth.getBalance(account, (err, balanceInWei) => {
+        const balanceInEther = window.web3.fromWei(balanceInWei).toString(10);
+        console.log(account, balanceInEther);
+      });
+    });
+  }
+
+  async getTodos() {
+    const { todoListInstance } = this.state;
+    const numTodos = await todoListInstance.getTotalNumTodos.call();
+    const pendingTodoPromises = [];
+    for (let i = 0; i < numTodos; i++) {
+      const todoPromise = todoListInstance.returnTodo.call(i);
+      pendingTodoPromises.push(todoPromise);
+    }
+    const todos = await Promise.all(pendingTodoPromises);
+    this.setState({ todos });
   }
 
   render() {
-    const { todos } = this.state
+    const {
+      todos,
+      todoListInstance: { createTodo, completeTodo },
+      account,
+    } = this.state;
+
     return (
       <div className="App">
         <nav className="navbar pure-menu pure-menu-horizontal">
-            <a href="#" className="pure-menu-heading pure-menu-link">My Todo List!</a>
+          <a href="#" className="pure-menu-heading pure-menu-link">
+            My Todo List!
+          </a>
         </nav>
 
         <main className="container">
@@ -38,18 +83,17 @@ class App extends Component {
               <h1>My todos!</h1>
               <p>Coming directly from my smart contract</p>
               <DisplayTodos
-                completeTodo={null /* completeTodoSmartContractFunc goes here */}
+                completeTodo={completeTodo}
                 todos={todos}
+                account={account}
               />
-              <CreateTodoBtn
-                createTodo={null /* createTodoSmartContractFunc goes here */}
-              />
+              <CreateTodoBtn createTodo={createTodo} account={account} />
             </div>
           </div>
         </main>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- src/Components/CreateTodoBtn.js
+++ src/Components/CreateTodoBtn.js
@@ -1,38 +1,46 @@
-import React, { Component } from 'react'
-import { Button, FormGroup, InputGroup, FormControl } from 'react-bootstrap'
+import React, { Component } from 'react';
+import { Button, FormGroup, InputGroup, FormControl } from 'react-bootstrap';
 
 export default class CreateToDoBtn extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      taskName: ""
-    }
+      taskName: '',
+    };
   }
 
-  handleInputChange(event){
-    this.setState({ taskName: event.target.value })
+  handleInputChange(event) {
+    this.setState({ taskName: event.target.value });
   }
 
-  async handleCreateTodo(){
-    const { taskName } = this.state
-    const { createTodo } = this.props
+  async handleCreateTodo() {
+    const { taskName } = this.state;
+    const { createTodo } = this.props;
 
     /* CREATE THE TODO HERE */
+    await createTodo(taskName, { from: this.props.account });
+    this.setState({ taskName: '' });
   }
 
-  render(){
-    const { taskName } = this.state
+  render() {
+    const { taskName } = this.state;
     return (
       <div className="col-md-6">
         <FormGroup>
           <InputGroup>
             <InputGroup.Button>
-              <Button onClick={this.handleCreateTodo.bind(this)} bsStyle="info">Create To Do</Button>
+              <Button onClick={this.handleCreateTodo.bind(this)} bsStyle="info">
+                Create To Do
+              </Button>
             </InputGroup.Button>
-            <FormControl value={taskName} onChange={this.handleInputChange.bind(this)} type="text" />
+            <FormControl
+              value={taskName}
+              onChange={this.handleInputChange.bind(this)}
+              type="text"
+            />
           </InputGroup>
         </FormGroup>
       </div>
-    )
+    );
   }
 }
